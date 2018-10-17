648a0593a2887cfceaa0915218fc802a29a5447f
mixmix <whimful@gmail.com>
2018-10-17 19:42:05 +1300

    fixes!

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const Connection = require('ssb-client')
 
+console.log('Connecting')
+
 Connection((err, server) => {
   if (err) {
     throw err
   }
+  console.log('Connection established')
 
   server.whoami((err, keys) => {
     if (err) console.log('could not get keys, got err', err)
-    else console.log(keys)
+    else console.log('whoami details:', keys)
 
+    console.log('disconnecting from server')
     server.close()
-    // close the connection to the server (not the server itself!)
-  }
+  })
 })
-
